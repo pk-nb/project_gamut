e571f277dbca5ec6ef0f3a832e0213b3ec0bd6a5
hpimentel <hpimentel@southern.edu>
2013-10-24 23:35:17 -0400

    Working with homepage and game view

--- app.js
+++ app.js
@@ -23,6 +23,9 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', function(req, res){
   res.render('index.jade', { title : 'Gamut - Home' });
 });
+app.post('/game', function(req, res){
+  res.render('game.jade', { title : 'Gamut - Game' });
+});
 
 
 // Socket Stuff
