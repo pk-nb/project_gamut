6ed74ad841310128e8e8a9ec1bc35836ca96d1ea
hpimentel <hpimentel@southern.edu>
2013-11-21 22:45:06 -0500

    Added chat functionality

--- public/js/dev/view.js
+++ public/js/dev/view.js
@@ -2,6 +2,10 @@ function printFeedback(string) {
   $('#debug').append("<p>" + string + "</p>");
 }
 
+function printChat(string) {
+  $('#chatEntries').append("<p>" + string + "</p>");
+}
+
 var clockTimes = 0;
 pubsub.subscribe('clock', function() {
   printFeedback("Ticktock " + clockTimes);
@@ -12,12 +16,15 @@ pubsub.subscribe('clock', function() {
 pubsub.subscribe('newGameRequested', function() {
   $('#newGameDiv').hide();
   $('#debug').show();
+  $('#chat').show();
 });
 
 /* View Binding
  *************************************/
 $(function() {
 
+  $("#chatSubmit").click(function() {sentMessage();});
+  $('#chat').hide();
   $("#sendPoke").click(function() { sendPoke() });
   $('#debug').hide();
 
@@ -38,6 +45,6 @@ $(function() {
     startGame(formParams);
   });
 
-
+  
 
 });
